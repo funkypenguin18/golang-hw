406edae04dac862c2e6eb77f80aac3add5438b3b
Oleg Danilov <ovdanilov@mts.ru>
2025-04-19 22:34:11 +0300

    hw2 completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var builder strings.Builder
+	lastChar := rune(0)
+	count := 0
+	for i, r := range s {
+		if r >= '0' && r <= '9' {
+			if unicode.IsDigit(r) { // Если текущий символ это цифра
+				// Если цифра идет в начале строки или после другой цифры, это ошибка
+				if i == 0 || i > 0 && unicode.IsDigit(rune(s[i-1])) {
+					return "", ErrInvalidString
+				}
+			}
+			if r == '0' {
+				continue //если 0 ничего не делаем
+			}
+			count := int(r - '0') // Конвертируем символ в цифру
+			builder.WriteString(strings.Repeat(string(lastChar), count))
+		} else {
+			// Проверка на наличие предыдущего символа для создания результата
+			if lastChar != 0 && count != 0 {
+				return "", ErrInvalidString
+			}
+			// Если это не первый символ и не цифра
+			if i > 0 && !unicode.IsDigit(rune(s[i-1])) {
+				builder.WriteRune(lastChar)
+			}
+			lastChar = r
+		}
+	}
+	// Если последний символ не 0 то его добавляем в строку
+	if lastChar != 0 && s[len(s)-1] != '0' {
+		builder.WriteRune(lastChar)
+	}
+	return builder.String(), nil
 }
